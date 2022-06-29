41c356157c8ec10e14b626cf392c5fd856bfb8bb
Andrew-Sem <avvyayvayva@gmail.com>
2022-06-29 10:38:23 +0300

    init commit

---
+++ src/store/store.ts
@@ -0,0 +1,7 @@
+import {applyMiddleware, createStore} from "redux";
+import thunk from "redux-thunk";
+import { configureStore } from '@reduxjs/toolkit'
+import {rootReducer} from "./reducers";
+
+
+export const store = configureStore({reducer: rootReducer, middleware: [thunk]})
